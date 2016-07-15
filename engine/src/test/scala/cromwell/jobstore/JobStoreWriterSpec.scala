2a2c2836c124fd54d7aa908c4284f7bdff2d8d59
Miguel Covarrubias <mcovarr@broadinstitute.org>
2016-07-15 11:45:19 -0400

    cleanup

--- engine/src/test/scala/cromwell/jobstore/JobStoreWriterSpec.scala
+++ engine/src/test/scala/cromwell/jobstore/JobStoreWriterSpec.scala
@@ -1,67 +1,79 @@
 package cromwell.jobstore
 
+import akka.actor.ActorRef
 import cromwell.CromwellTestkitSpec
 import cromwell.core.WorkflowId
 import cromwell.jobstore.JobStoreService.{JobStoreWriteSuccess, RegisterJobCompleted, RegisterWorkflowCompleted}
-import org.scalatest.Matchers
+import org.scalatest.{BeforeAndAfter, Matchers}
 
 import scala.concurrent.duration._
 import language.postfixOps
 import scala.concurrent.{ExecutionContext, Future}
 
-class JobStoreWriterSpec extends CromwellTestkitSpec with Matchers {
+class JobStoreWriterSpec extends CromwellTestkitSpec with Matchers with BeforeAndAfter {
+  
+  var database: WriteCountingJobStoreDatabase = _
+  var jobStoreWriter: ActorRef = _
+  var workflowId: WorkflowId = _
+  val successResult: JobResult = JobResultSuccess(Some(0), Map.empty)
+
+  before {
+    database = WriteCountingJobStoreDatabase.makeNew
+    jobStoreWriter = system.actorOf(JobStoreWriterActor.props(database))
+  }
+
+  private def registerCompletions(attempts: Int): Unit = {
+    0 until attempts foreach { a => jobStoreWriter ! RegisterJobCompleted(jobKey(attempt = a), successResult) }
+  }
+
+  private def jobKey(attempt: Int): JobStoreKey = JobStoreKey(workflowId, s"call.fqn", None, attempt)
+
+  private def assertWriteSuccess(key: JobStoreKey, result: JobResult): Unit = {
+    key.workflowId shouldBe workflowId
+    key.callFqn shouldBe "call.fqn"
+    key.index shouldBe None
+    result shouldBe successResult
+  }
+
+  private def assertDb(totalWritesCalled: Int, jobCompletionsRecorded: Int, workflowCompletionsRecorded: Int): Unit = {
+    database.totalWritesCalled shouldBe totalWritesCalled
+    database.jobCompletionsRecorded shouldBe jobCompletionsRecorded
+    database.workflowCompletionsRecorded shouldBe workflowCompletionsRecorded
+  }
+
+  private def assertReceived(allowWorkflowCompleted: Boolean): Unit = {
+    val received = receiveN(3, 10 seconds)
+    received foreach {
+      case JobStoreWriteSuccess(RegisterJobCompleted(key: JobStoreKey, result)) => assertWriteSuccess(key, result)
+      case JobStoreWriteSuccess(RegisterWorkflowCompleted(id)) if allowWorkflowCompleted => id shouldBe workflowId
+      case message => fail(s"Unexpected response message: $message")
+    }
+  }
 
   "JobStoreWriter" should {
     "be able to collapse writes together if they arrive while a database access is ongoing" in {
-      val database = WriteCountingJobStoreDatabase.makeNew
-      val jsw = system.actorOf(JobStoreWriterActor.props(database))
-      val wfid = WorkflowId.randomId()
-      def jobKey(attempt: Int): JobStoreKey = JobStoreKey(wfid, s"call.fqn", None, attempt)
-      val jobResult: JobResult = JobResultSuccess(Some(0), Map.empty)
 
-      jsw ! RegisterJobCompleted(jobKey(0), jobResult)
-      jsw ! RegisterJobCompleted(jobKey(1), jobResult)
-      jsw ! RegisterJobCompleted(jobKey(2), jobResult)
-      val received = receiveN(3, 10 seconds)
-      received foreach {
-        case JobStoreWriteSuccess(RegisterJobCompleted(JobStoreKey(jsk_wfid, jsk_callfqn, jsk_index, _), result)) =>
-          jsk_wfid shouldBe wfid
-          jsk_callfqn shouldBe "call.fqn"
-          jsk_index shouldBe None
-          result shouldBe jobResult
-        case message => fail(s"Unexpected response message: $message")
-      }
+      registerCompletions(attempts = 3)
+      assertReceived(allowWorkflowCompleted = false)
 
-      database.totalWritesCalled shouldBe 2
-      database.jobCompletionsRecorded shouldBe 3
-      database.workflowCompletionsRecorded shouldBe 0
+      assertDb(
+        totalWritesCalled = 2,
+        jobCompletionsRecorded = 3,
+        workflowCompletionsRecorded = 0
+      )
     }
 
     "be able to skip job-completion writes if the workflow completes, but still respond appropriately" in {
-      val database = WriteCountingJobStoreDatabase.makeNew
-      val jsw = system.actorOf(JobStoreWriterActor.props(database))
-      val wfid = WorkflowId.randomId()
-      def jobKey(attempt: Int): JobStoreKey = JobStoreKey(wfid, s"call.fqn", None, attempt)
-      val jobResult: JobResult = JobResultSuccess(Some(0), Map.empty)
 
-      jsw ! RegisterJobCompleted(jobKey(0), jobResult)
-      jsw ! RegisterJobCompleted(jobKey(1), jobResult)
-      jsw ! RegisterWorkflowCompleted(wfid)
-      val received = receiveN(3, 10 seconds)
-      received foreach {
-        case JobStoreWriteSuccess(RegisterJobCompleted(JobStoreKey(jsk_wfid, jsk_callfqn, jsk_index, _), result)) =>
-          jsk_wfid shouldBe wfid
-          jsk_callfqn shouldBe "call.fqn"
-          jsk_index shouldBe None
-          result shouldBe jobResult
-        case JobStoreWriteSuccess(RegisterWorkflowCompleted(rwc_wfid)) =>
-          rwc_wfid shouldBe wfid
-        case message => fail(s"Unexpected response message: $message")
-      }
+      registerCompletions(attempts = 2)
+      jobStoreWriter ! RegisterWorkflowCompleted(workflowId)
+      assertReceived(allowWorkflowCompleted = true)
 
-      database.totalWritesCalled shouldBe 2
-      database.jobCompletionsRecorded shouldBe 1
-      database.workflowCompletionsRecorded shouldBe 1
+      assertDb(
+        totalWritesCalled = 2,
+        jobCompletionsRecorded = 1,
+        workflowCompletionsRecorded = 1
+      )
     }
   }
 }
